9c85c68a7367c0686ba11dcada8aeefa9cbe5a59
Andrey Flek <a.flek@avia-centr.ru>
2016-11-26 23:01:56 +0300

    Added react-rails Gem

--- Gemfile
+++ Gemfile
@@ -50,3 +50,4 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 #ADDED Gems
 gem 'bootstrap', '~> 4.0.0.alpha5'
+gem 'react-rails'
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,4 +15,7 @@
 //= require turbolinks
 // ADDED bootstrap-sprockets
 //= require bootstrap-sprockets
+//= require react
+//= require react_ujs
+//= require components
 //= require_tree .
